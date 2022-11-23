55dd4e1b29ecd943bfe30e173448d647454d292b
Esthephanie Beltran <18170455@itcul;iacan.edu.mx>
2022-11-23 09:29:09 -0700

    Initial Commit

---
+++ ExamenFinal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExamenFinal
+//
+//  Created by CCDM21 on 23/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExamenFinal/ExamenFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenFinalApp.swift
+//  ExamenFinal
+//
+//  Created by CCDM21 on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ExamenFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
