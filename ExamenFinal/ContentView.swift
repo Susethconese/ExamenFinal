849944b60ee486539a58ee4f37a417a803edd6aa
Esthephanie Beltran <18170455@itcul;iacan.edu.mx>
2022-11-24 10:17:57 -0700

    agregar producto y mostrar

--- ExamenFinal/ContentView.swift
+++ ExamenFinal/ContentView.swift
@@ -6,16 +6,76 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    let coreDM: CoreDataManager
+    @State var clave_viga = " "
+    @State var clave_obra = " "
+    @State var peso = ""
+    @State var longitud = ""
+    @State var material = " "
+    @State var vigArray = [Viga]()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            TextField("Clave de la viga", text: $clave_viga)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Clave de la obra", text: $clave_obra)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Peso de la viga", text: $peso)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Longitud de la viga", text: $longitud)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Material de la viga", text: $material)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Save"){
+                coreDM.guardarViga(clave_viga: clave_viga, clave_obra: clave_obra, peso: peso, longitud: longitud, material: material)
+                mostrarVigas()
+                clave_viga = " "
+                clave_obra = " "
+                peso = ""
+                longitud = ""
+                material = " "
+                
+            }
+            List{
+                ForEach(vigArray, id: \.self){
+                    vig in
+                    VStack{
+                        Text(vig.clave_viga ?? "")
+                        Text(vig.clave_obra ?? "")
+                        Text(vig.peso ?? "")
+                        Text(vig.longitud ?? "")
+                        Text(vig.material ?? "")
+                        
+                    }
+                }
+                .onDelete(perform: {
+                    IndexSet in
+                    IndexSet.forEach({ index in
+                        let viga = vigArray[index]
+                        coreDM.borrarViga(viga: viga)
+                        mostrarVigas()
+                        
+                    })
+                })
+            }
+            Spacer()
+        }.padding()
+            .onAppear(perform: {
+                mostrarVigas()
+            })
+    }
+    func mostrarVigas(){
+        vigArray = coreDM.leerTodasLasVigas()
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(coreDM: CoreDataManager())
     }
 }
--- ExamenFinal/CoreDataManager.swift
+++ ExamenFinal/CoreDataManager.swift
@@ -24,7 +24,7 @@ class CoreDataManager{
         
         
     }
-    func guardarViga(clave_viga: Int64, clave_obra: Int64, peso: Double, longitud: Double, material: String){
+    func guardarViga(clave_viga:  String, clave_obra: String, peso: String, longitud: String, material: String){
         let viga = Viga(context: persistentContainer.viewContext)
         viga.clave_viga = clave_viga
         viga.clave_obra = clave_obra
